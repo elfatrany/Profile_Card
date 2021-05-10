08639020f5b28990fec03142b72aabfe575a2633
Sammy Elfatrany <sammyelfatrany@Sammys-MacBook-Air.local>
2021-05-10 18:38:40 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import PROFILE from './images/img_avatar.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import Display from "./Component/Display.js"
+
+
+const linkedin = <FontAwesomeIcon icon={faLinkedin} />
+const twitter =  <FontAwesomeIcon icon={faTwitter} />
+const element = <FontAwesomeIcon icon={faHeart} />
+
 
 function App() {
+
+const [intro , setIntro] = useState("My Name is Sammy")
+
+const handleName =()=>{
+  setIntro("My Name is Sammy")
+}
+
+const handleSkills =()=>{
+  setIntro("I'm a Frontend Developer")
+}
+
+const [count, setCount] = useState(0);
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Tile">
+        <img src={PROFILE} alt="profile" className="Photo"/>
+        <h1>Hi!</h1>
+        <Display intro = {intro}/>
+        <button onClick ={handleName} className="button">Name</button>
+        <button onClick = {handleSkills} className="button">Skills</button>
+        <p>{count} <span onClick={() => setCount(count +1)}>{element}</span></p>
+        <div className="social">
+        <p>{twitter}    {linkedin}</p>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/Component/Display.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Display({intro}) {
+    return (
+        <>
+            <p>{intro}</p>
+        </>
+    )
+}
+
+export default Display;
